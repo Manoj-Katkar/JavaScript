3f97d203d1b24bbd5d529d7515f0fbafdb495005
Manoj-Katkar <education2001manojkatkar@gmail.com>
2023-11-30 17:22:12 +0530

    All basics types of the variables learned

---
+++ App.js
@@ -0,0 +1,24 @@
+console.log("Hello Now I am in App.js changed");
+
+var name = "Jaishankar";
+console.log(name);
+
+var name = "V...........";  //Re-Declare the var variable 
+// console.log(name);
+
+
+//using let 
+let age = 24;
+// console.log(age);
+
+// let age = 45; // it is not possible
+
+age = 45;
+console.log(age);
+
+// const:
+const b = 200;
+// b = 100;
+
+console.log(b);
+
