1e87234d1276689f0daf2b202325bacb8ea25d44
Manoj-Katkar <education2001manojkatkar@gmail.com>
2023-12-01 19:51:11 +0530

    Done the revision with all javascript outpup ptinting statements

--- day 2/app.js
+++ day 2/app.js
@@ -1,8 +1,56 @@
-console.log("Hello Its Browser side");
+const a = "2044";
+
+console.log(a);
+
+console.log(125245);
+console.log("Hello this is server side JavaScript------------!! yewwwwwwwwwwwwwwww");
+
+
+// now suppose I want to print the data on the webpage 
+// so for that I have to  use 
+// document.write("Hello Welcome to the 2nd class of the javascrit-------------started");
+// document.writeln("Hello Welcome to the 2nd class of the javascrit-------------started");
+
+
+// now if I want to print the data or to show the data on the webpage but I want the line break 
+// so that I can Achieve with the help of the reak tag of the html  = <br/>
+// document.write("Hello Welcome to the 2nd class of the javascrit-------------started" + "<br/>");   //concatiinate with the break tag in the string format only then it will work otherwise it will not work 
+
+
+// now if I want the gap means space between the two data that I am displaing on the webpage 
+// document.writeln("JavaScript Enjoying----!");
+
+// now lets see if I want to show to the user some alerts then How I can show it 
+// so that I can Achieve with using the alert() popup method (it does  not return anything)
+
+// alert("Hey Do you want to continue to this website it might suspicious");
+
+
+// now supoose I want that in my webaapplication user should click on the terms and condition or give the confirmation  or you are 18+ like this statements 
+// it will also show the pop up 
+// but it will return the boolean value means if use click on the ok = true else false
+// let isClick = confirm("Are you 18+");
+// console.log(isClick);
+
+
+
+// now I want to user to enter some input 
+// so that I can Achieve using the prompt() method
+// it return the null and String values only then you pass any number data or anything it will convert that into the String and return it 
+// it is  used for user to enter some data 
+
+// let return_value = prompt("Hello");
+// console.log(return_value);
+
+
+
+
+
+// console.log("Hello Its Browser side");
 // alert("I am alert popup");
 
 // let isClick = confirm("Are you shure You want to Enter ?");
 // console.log(isClick);
 
-let age = prompt("Enter your Age");
-console.log(age);
+// let age = prompt("Enter your Age");
+// console.log(age);
