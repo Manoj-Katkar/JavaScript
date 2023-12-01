3f469e59505b016ace3f5ee06c66d96b40633962
Manoj-Katkar <education2001manojkatkar@gmail.com>
2023-12-01 14:51:51 +0530

    learned literals and output methods of javascript

---
+++ day 2/app.js
@@ -0,0 +1,8 @@
+console.log("Hello Its Browser side");
+// alert("I am alert popup");
+
+// let isClick = confirm("Are you shure You want to Enter ?");
+// console.log(isClick);
+
+let age = prompt("Enter your Age");
+console.log(age);
