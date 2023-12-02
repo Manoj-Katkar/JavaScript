3a2185a5f1802a75ba92f731ebcaebd03e37503b
Manoj-Katkar <education2001manojkatkar@gmail.com>
2023-12-02 14:51:37 +0530

    learned primitive data types but practice required

---
+++ day3 primitive datatypes/App.js
@@ -0,0 +1,30 @@
+let name = `Dhoni`;
+console.log(typeof name);
+
+
+name = null;
+console.log(name);
+
+let b;
+console.log(b); 
+
+//~NaN (Not A Number datatype)
+
+console.log(20*3);
+
+console.log(10 + "str");
+console.log(10 - "manoj");
+
+let num1 = 30;
+console.log(isNaN(num1));  // study this one 
+
+
+//~BigInt
+// if we want to store the more than number range value then  we will use
+// it must be suffix with n
+
+let num2 = 10000n;
+console.log(num2);
+console.log(typeof num2);
+
+
