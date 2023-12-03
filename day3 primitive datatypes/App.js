13989b0a783645ad5457e991686d7f94b0801a96
Manoj-Katkar <education2001manojkatkar@gmail.com>
2023-12-03 22:54:12 +0530

    Revision of the primitive Datatypes is done

--- day3 primitive datatypes/App.js
+++ day3 primitive datatypes/App.js
@@ -1,29 +1,91 @@
-let name = `Dhoni`;
-console.log(typeof name);
+// first 
+//~Number 
+
+// let a = 20;
+// console.log(a);
+
+// a = 20.3654;
+// console.log(a);
 
 
-name = null;
-console.log(name);
+//~String
+// ~first one using the Double inverted codes = " "
+// let name = "Manoj Katkar";
+// console.log(name);
 
-let b;
-console.log(b); 
+// there are the three ways to declare the string datatype 
+// ~first one using the Double inverted codes = " "
+// ~second one using the Single inverted Codes = ' '
+// ~third one using the using pactics = ``
 
-//~NaN (Not A Number datatype)
+// ~second one using the Single inverted Codes = ' '
+// name = 'Modi';
+// console.log(name);
+
+
+
+// ~third one using the using pactics = ``
+// using the pactics I can do tghe interpolation on the String 
+// ~Interpolation = Means Putting the variables or the expressions into the Strings we can use multiple also 
+
+// let primeMinister = `Rahul Gandhi , ${100 + 600} ${"Manoj "}  ${580}`;
+// console.log(primeMinister);
+
+// const Name = "Alice";
+// const greeting = `Hello, ${Name}!`;
+// console.log(greeting);
+
+
+
+
+
+// ~boolean
+
+// let isFollow = false;
+// console.log(isFollow);
+
+
+
+
+
+// ~null
+// let c = null;
+// console.log(c);
+
+
+//~undefined
+
+// let b;
+// console.log(b); 
+
+// //~NaN (Not A Number datatype)
+
+isNaN(NaN);  // true
+isNaN(5);    // false
 
 console.log(20*3);
 
-console.log(10 + "str");
-console.log(10 - "manoj");
+let val = 10 + "str";
+console.log(val);
 
-let num1 = 30;
-console.log(isNaN(num1));  // study this one 
+console.log("==============================");
+
+// console.log(10 - "manoj");
+
+// let num1 = 30;
+// console.log(isNaN(num1));  // study this one 
+
+
+let ans = 10/"Hello";
+console.log(ans);
+console.log(typeof ans);  //~but the type of the NaN is number
 
 
 //~BigInt
 // if we want to store the more than number range value then  we will use
 // it must be suffix with n
 
-let num2 = 10000n;
+let num2 = 10000000000000000000000000648560n;
 console.log(num2);
 console.log(typeof num2);
 
