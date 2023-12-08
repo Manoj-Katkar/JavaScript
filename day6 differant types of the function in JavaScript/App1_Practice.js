701ed12fcbfcc870bc0e93c70d974968469c66f3
Manoj-Katkar <education2001manojkatkar@gmail.com>
2023-12-09 00:16:05 +0530

    Revised the only 5 functions till the higher order function remaining do it morning

---
+++ day6 differant types of the function in JavaScript/App1_Practice.js
@@ -0,0 +1,35 @@
+// !Ananoymous function 
+
+
+// var add = function(a, b) {
+//     return a + b;
+// };
+
+// console.log(add(10 , 50)); 
+
+// var myFunction = function() {
+//     console.log("This is an anonymous function.");
+//   };
+  
+//   // You can then call the function using its variable name
+//   myFunction();
+
+
+// !Lets Learn The Higher Order Functions 
+
+// ~ the function which takes the function as an argument is known as t he higher order function
+// call back function
+
+
+function x(){
+    console.log("Hi I am Call Back Function");
+}
+
+// Higher order function
+
+function y(x){
+    //  return x();
+    x();
+}
+
+y(x);
