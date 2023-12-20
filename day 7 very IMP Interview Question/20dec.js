0106404d946e308de62688dd5c71fe11973fe93c
Manoj-Katkar <education2001manojkatkar@gmail.com>
2023-12-20 14:51:32 +0530

    learned the DOM some part

---
+++ day 7 very IMP Interview Question/20dec.js
@@ -0,0 +1,14 @@
+console.log(document);
+console.dir(document);
+
+let a = console.log(document.body.children);
+console.log(a);
+
+let b = document.body.children;
+console.log(Array.isArray(b));
+console.log(b);
+
+let h1 = b[0];
+let link = document.get
+
+console.log(a[0]);
